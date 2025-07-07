c8cbda6b8229d2317b274a3bbc41544713c74b89
Akhil Krishna KJ <akhilkrishnakj@example.com>
2025-07-07 17:56:30 +0530

    fixng bugs

--- client/src/pages/Admin/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard.jsx
@@ -5,7 +5,6 @@ import {
 } from 'lucide-react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import ReactApexChart from 'react-apexcharts';
 import { ResponsiveContainer } from 'recharts';
 import FullPageLoader from '../../components/FullPageLoader';
 
